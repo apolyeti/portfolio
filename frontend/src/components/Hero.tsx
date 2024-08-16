7863af68d33f9381f98f6c4af3aba8dee84a8b57
apolyeti <azhandarveen@gmail.com>
2024-08-15 17:11:50 -0700

    fix: issues

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -1,8 +1,17 @@
 
 export default function Hero() {
     return (
-        <h1>
-            Hi
-        </h1>
+        <div className="hero-container">
+            <div className="vstack">
+                <div className="hstack">
+                    <h1 className="w-11">
+                        as
+                    </h1>
+                    <h1>
+                        as
+                    </h1>
+                </div>
+            </div>
+        </div>
     )
 }
