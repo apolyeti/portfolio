b560a194588ae112cbdf883f5dd4c6d11d0018e2
apolyeti <azhandarveen@gmail.com>
2024-08-18 15:31:22 -0700

    build: use rollupOptions.output.manualChunks to improve chunking on build

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,7 @@ import {
     BrowserRouter as Router, Route, Routes 
 }   from 'react-router-dom';
 import App                          from './App';
-import Contact                      from './pages/contact';
+import Contact                      from './pages/Contact';
 import './index.css'
 import 'vite/modulepreload-polyfill'
 
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,6 +9,20 @@ export default defineConfig({
         manifest: true,
         rollupOptions: {
             input: 'src/main.tsx',
-        }
-    }
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                    }
+                    if (id.includes('/src/components/')) {
+                        return 'components';
+                    }
+                    if (id.includes('/src/pages/')) {
+                        return 'pages';
+                    }
+                }
+            },
+        },
+        chunkSizeWarningLimit: 1024,
+    },
 })
